dfb36bce003caa2a595a7186fe589e5c627d29d2
Siddhant Prakash Kadam <86238624+sidhantkadam@users.noreply.github.com>
2024-11-08 16:31:15 +0530

    Created spring cloud gateway for three different services to manage traffic

---
+++ Spring_Cloud_Gateway/src/test/java/com/sid/project/Spring_Cloud_Gateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.project.Spring_Cloud_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
